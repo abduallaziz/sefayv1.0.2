3d7bd14bac25eaf51e8c3149f82888c08d1be180
abduallaziz <abduallaziz.altyar@gmail.com>
2026-06-10 13:07:34 +0300

    fix: default locale ar, both locales available

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,6 +1,7 @@
 import { defineRouting } from 'next-intl/routing'
 
 export const routing = defineRouting({
-  locales: ['en', 'ar'],
-  defaultLocale: 'en'
+  locales: ['ar', 'en'],
+  defaultLocale: 'ar',
+  localePrefix: 'always'
 })
