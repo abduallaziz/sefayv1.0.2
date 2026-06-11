5f78527d0db13ab723bb91670b88f203e2305e8c
abduallaziz <abduallaziz.altyar@gmail.com>
2026-06-12 01:36:33 +0300

    fix: force rebuild for customers i18n namespace

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,6 +1,6 @@
+// cache-bust-v2
 import { getRequestConfig } from 'next-intl/server'
 import { routing } from './routing'
-
 export default getRequestConfig(async ({ requestLocale }) => {
   let locale = await requestLocale
 
